14b5c80156c14c7b7dfeb42cc6a531606750c592
Raphael Townshend <raphtown@stanford.edu>
2020-04-14 15:57:03 -0700

    Add file utility

---
+++ atom3d/util/file.py
@@ -0,0 +1,21 @@
+"""File-related utilities."""
+import os
+import subprocess
+
+
+def find_files(path, suffix, relative=None):
+    """
+    Find files in path, with given suffix.
+
+    Optionally can specify path we want to perform the find relative to.
+    """
+    if not relative:
+        find_cmd = "find {:} -name '*.{:}'".format(path, suffix)
+    else:
+        find_cmd = "cd {:}; find . -name '*.{:}' | cut -d '/' -f 2-" \
+            .format(path, suffix)
+    out = subprocess.Popen(
+        find_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        cwd=os.getcwd(), shell=True)
+    (stdout, stderr) = out.communicate()
+    return stdout.decode().split()
