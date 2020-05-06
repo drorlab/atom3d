ca23d2af5280845aa119918d87f3fd996ff0caef
martinvoegele <martinvoegele1989@gmail.com>
2020-05-06 16:21:53 -0700

    cormorant run files

--- examples/cormorant/prepare-qm9.sh
+++ examples/cormorant/prepare-qm9.sh
@@ -6,6 +6,7 @@ IDXDIR=../../data/qm9/splits
 
 mkdir $NPZDIR
 mkdir $NPZDIR/qm9
+mkdir $IDXDIR
 
-python ../../atom3d/mpp/convert_qm9_from_sdf_to_npz.py $RAWDIR/gdb9.sdf.csv $RAWDIR/gdb9.sdf $NPZDIR/qm9 -i $IDXDIR
+python ../../atom3d/mpp/convert_qm9_from_sdf_to_npz.py $RAWDIR $NPZDIR/qm9 -i $IDXDIR
 
