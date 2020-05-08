f0a83484e45bbe12705a91f76f7c3039dfb186f1
Raphael Townshend <raphtown@stanford.edu>
2020-05-08 13:50:17 -0700

    Refactor splitting and move sequence code to separate utility file

---
+++ atom3d/util/sequence.py
@@ -0,0 +1,142 @@
+"""Code relating to biomolecular sequence."""
+import os
+import subprocess
+
+from Bio import SeqIO
+from Bio.Blast.Applications import NcbiblastpCommandline
+import tqdm
+
+import atom3d.util.datatypes as dt
+
+
+def find_similar(chain_sequences, blast_db, cutoff, dataset_size):
+    """
+    Find all other pdbs that have sequence identity greater than cutoff.
+    """
+    sim = set()
+    for chain, s in chain_sequences:
+        blastp_cline = NcbiblastpCommandline(
+            db=blast_db,
+            outfmt="10 nident sacc",
+            num_alignments=dataset_size,
+            cmd='/usr/local/ncbi/blast/bin/blastp')
+        out, err = blastp_cline(stdin=s)
+
+        for res in out.split():
+            nident, match = res.split(',')
+            ref_pdb = match.split('_')[0]
+            seq_id = float(nident) / len(s)
+            if seq_id >= cutoff:
+                sim.add(ref_pdb)
+
+    return sim
+
+
+####################################
+# Sequence-level clustering code
+####################################
+
+
+def get_pdb_clusters(id_level, pdb_ids=None):
+    """
+    Downloads pre-calculated clusters from PDB at given cutoff.
+    Returns dictionaries mapping PDB IDs to cluster IDs and vice versa.
+    """
+    id_level = int(id_level * 100)
+    if id_level not in [30, 40, 50, 70, 90, 95, 100]:
+        raise Exception(
+            'invalid invalid identity cutoff. '
+            'possible values = 30,40,50,70,90,95,100')
+    print('getting clusters from PDB...')
+    subprocess.call(
+        'wget ftp://resources.rcsb.org/sequence/clusters/bc-{}.out'.format(
+            id_level),
+        shell=True)
+    pdb2cluster = {}
+    cluster2pdb = {}
+    with open(f'bc-{id_level}.out') as f:
+        for i, line in enumerate(f):
+            cluster2pdb[i] = set()
+            pdbs = line.strip().split()
+            pdbs = [p[:4].lower() for p in pdbs]
+            for pdb in pdbs:
+                if pdb_ids is not None and pdb not in pdb_ids:
+                    continue
+                if pdb not in pdb2cluster:
+                    pdb2cluster[pdb] = set()
+                pdb2cluster[pdb].add(i)
+                cluster2pdb[i].add(pdb)
+
+    os.system(f'rm bc-{id_level}.out')
+
+    return pdb2cluster, cluster2pdb
+
+
+def find_cluster_members(pdb, clusterings):
+    """
+    Find all other pdbs that co-occur in a cluster with a given pdb
+    """
+    pdb2cluster, cluster2pdb = clusterings
+    clusters = pdb2cluster[pdb]
+    all_pdbs = set()
+    for clust in clusters:
+        pdbs = cluster2pdb[clust]
+        all_pdbs = all_pdbs.union(pdbs)
+    return all_pdbs
+
+
+####################################
+# useful functions
+####################################
+
+def write_to_blast_db(all_chain_sequences, blast_db):
+    """Write provided pdb dataset to blast db, for use with BLAST.
+
+    Inputs:
+    - all_chain_sequences: list of (pdb_id, chain_sequences)
+    """
+    print('writing chain sequences to BLAST db')
+    flat_map = {}
+    for cs in all_chain_sequences:
+        for (chain, seq) in cs:
+            flat_map[chain] = seq
+
+    write_fasta(flat_map, blast_db)
+
+    subprocess.check_output(
+        'makeblastdb -in ' +
+        blast_db +
+        ' -dbtype prot',
+        shell=True)
+
+
+def get_chain_sequences(pdb_file):
+    """
+    Returns list of chain sequences from PDB file
+    """
+#     fname = os.path.join(path, pdb, pdb + '_protein.pdb')
+    chain_seqs = []
+    for seq in SeqIO.parse(pdb_file, 'pdb-atom'):
+        try:
+            pdb_id = seq.idcode
+        except BaseException:
+            pdb_id = os.path.basename(pdb_file).rstrip('.pdb')
+        chain = pdb_id + '_' + seq.annotations['chain']
+        chain_seqs.append((chain, str(seq.seq)))
+    return chain_seqs
+
+
+def get_all_chain_sequences(pdb_dataset):
+    """Return list of tuples of (pdb_code, chain_sequences) for PDB dataset."""
+    return [(dt.get_pdb_code(p), get_chain_sequences(p))
+            for p in tqdm.tqdm(pdb_dataset)]
+
+
+def write_fasta(seq_dict, outfile):
+    """
+    Takes dictionary of sequences keyed by id and writes to fasta file
+    """
+    with open(outfile, 'w') as f:
+        for i, s in seq_dict.items():
+            f.write('>' + i + '\n')
+            f.write(s + '\n')
--- atom3d/util/splits.py
+++ atom3d/util/splits.py
@@ -1,18 +1,34 @@
-import os
-import subprocess
+"""Functions for splitting data into test, validation, and training sets."""
 import sys
 
 import numpy as np
-import tqdm
-from Bio.Blast.Applications import NcbiblastpCommandline
 
-from Bio import SeqIO
+import atom3d.util.sequence as seq
+
 sys.path.append('../..')
 
-import atom3d.util.datatypes as dt
+
+def read_split_file(split_file):
+    """
+    Read text file with pre-defined split, returning list of examples.
+
+    One example per row in text file.
+    """
+    with open(split_file) as f:
+        # file may contain integer indices or string identifiers (e.g. PDB
+        # codes)
+        lines = f.readlines()
+        try:
+            split = [int(x.strip()) for x in lines]
+        except ValueError:
+            split = [x.strip() for x in lines]
+    return split
 
 
-# Splits data into test, validation, and training sets.
+####################################
+# split randomly
+####################################
+
 
 def random_split(dataset_size, train_split=None, vali_split=0.1,
                  test_split=0.1, shuffle=True, random_seed=None, exclude=None):
@@ -70,6 +86,11 @@ def random_split(dataset_size, train_split=None, vali_split=0.1,
     return indices_test, indices_vali, indices_train
 
 
+####################################
+# split by time
+####################################
+
+
 def time_split(data, val_years, test_years):
     """
     Splits data into train, val, test by year.
@@ -93,23 +114,6 @@ def time_split(data, val_years, test_years):
     test_set = test['pdb'].tolist()
 
     return train_set, val_set, test_set
-
-
-def read_split_file(split_file):
-    """
-    Read text file with pre-defined split, returning list of examples.
-
-    One example per row in text file.
-    """
-    with open(split_file) as f:
-        # file may contain integer indices or string identifiers (e.g. PDB
-        # codes)
-        lines = f.readlines()
-        try:
-            split = [int(x.strip()) for x in lines]
-        except ValueError:
-            split = [x.strip() for x in lines]
-    return split
 
 
 ####################################
@@ -152,7 +156,7 @@ def cluster_split(all_chain_sequences, cutoff, val_split=0.1,
     np.random.shuffle(all_chain_sequences)
     pdb_ids = [p for (p, _) in all_chain_sequences]
 
-    clusterings = get_pdb_clusters(cutoff, pdb_ids)
+    clusterings = seq.get_pdb_clusters(cutoff, pdb_ids)
 
     print('generating validation set...')
     val_set, all_chain_sequences = create_cluster_split(
@@ -169,54 +173,6 @@ def cluster_split(all_chain_sequences, cutoff, val_split=0.1,
     return train_set, val_set, test_set
 
 
-def get_pdb_clusters(id_level, pdb_ids=None):
-    """
-    Downloads pre-calculated clusters from PDB at given cutoff.
-    Returns dictionaries mapping PDB IDs to cluster IDs and vice versa.
-    """
-    id_level = int(id_level * 100)
-    if id_level not in [30, 40, 50, 70, 90, 95, 100]:
-        raise Exception(
-            'invalid invalid identity cutoff. '
-            'possible values = 30,40,50,70,90,95,100')
-    print('getting clusters from PDB...')
-    subprocess.call(
-        'wget ftp://resources.rcsb.org/sequence/clusters/bc-{}.out'.format(
-            id_level),
-        shell=True)
-    pdb2cluster = {}
-    cluster2pdb = {}
-    with open(f'bc-{id_level}.out') as f:
-        for i, line in enumerate(f):
-            cluster2pdb[i] = set()
-            pdbs = line.strip().split()
-            pdbs = [p[:4].lower() for p in pdbs]
-            for pdb in pdbs:
-                if pdb_ids is not None and pdb not in pdb_ids:
-                    continue
-                if pdb not in pdb2cluster:
-                    pdb2cluster[pdb] = set()
-                pdb2cluster[pdb].add(i)
-                cluster2pdb[i].add(pdb)
-
-    os.system(f'rm bc-{id_level}.out')
-
-    return pdb2cluster, cluster2pdb
-
-
-def find_cluster_members(pdb, clusterings):
-    """
-    Find all other pdbs that co-occur in a cluster with a given pdb
-    """
-    pdb2cluster, cluster2pdb = clusterings
-    clusters = pdb2cluster[pdb]
-    all_pdbs = set()
-    for clust in clusters:
-        pdbs = cluster2pdb[clust]
-        all_pdbs = all_pdbs.union(pdbs)
-    return all_pdbs
-
-
 def create_cluster_split(all_chain_sequences, clusterings, cutoff, split_size,
                          min_fam_in_split):
     """
@@ -229,7 +185,7 @@ def create_cluster_split(all_chain_sequences, clusterings, cutoff, split_size,
     while len(split) < split_size:
         (rand_id, rand_cs) = all_chain_sequences[idx]
         split.add(rand_id)
-        hits = find_cluster_members(rand_id, clusterings)
+        hits = seq.find_cluster_members(rand_id, clusterings)
         # ensure that at least min_fam_in_split families in each split
         if len(hits) > split_size / min_fam_in_split:
             idx += 1
@@ -278,7 +234,7 @@ def identity_split(
 
     """
     if blast_db is None:
-        write_to_blast_db(all_chain_sequences, 'blast_db')
+        seq.write_to_blast_db(all_chain_sequences, 'blast_db')
         blast_db = 'blast_db'
 
     if random_seed is not None:
@@ -307,29 +263,6 @@ def identity_split(
     return train_set, val_set, test_set
 
 
-def find_similar(chain_sequences, blast_db, cutoff, dataset_size):
-    """
-    Find all other pdbs that have sequence identity greater than cutoff.
-    """
-    sim = set()
-    for chain, seq in chain_sequences:
-        blastp_cline = NcbiblastpCommandline(
-            db=blast_db,
-            outfmt="10 nident sacc",
-            num_alignments=dataset_size,
-            cmd='/usr/local/ncbi/blast/bin/blastp')
-        out, err = blastp_cline(stdin=seq)
-
-        for res in out.split():
-            nident, match = res.split(',')
-            ref_pdb = match.split('_')[0]
-            seq_id = float(nident) / len(seq)
-            if seq_id >= cutoff:
-                sim.add(ref_pdb)
-
-    return sim
-
-
 def create_identity_split(all_chain_sequences, cutoff, split_size,
                           min_fam_in_split):
     """
@@ -343,7 +276,7 @@ def create_identity_split(all_chain_sequences, cutoff, split_size,
     while len(split) < split_size:
         (rand_id, rand_cs) = all_chain_sequences[idx]
         split.add(rand_id)
-        hits = find_similar(rand_cs, 'blast_db', cutoff, dataset_size)
+        hits = seq.find_similar(rand_cs, 'blast_db', cutoff, dataset_size)
         # ensure that at least min_fam_in_split families in each split
         if len(hits) > split_size / min_fam_in_split:
             idx += 1
@@ -357,60 +290,3 @@ def create_identity_split(all_chain_sequences, cutoff, split_size,
         pdb_ids.pop(loc)
 
     return split, all_chain_sequences
-
-
-####################################
-# useful functions
-####################################
-
-def write_to_blast_db(all_chain_sequences, blast_db):
-    """Write provided pdb dataset to blast db, for use with BLAST.
-
-    Inputs:
-    - all_chain_sequences: list of (pdb_id, chain_sequences)
-    """
-    print('writing chain sequences to BLAST db')
-    flat_map = {}
-    for cs in all_chain_sequences:
-        for (chain, seq) in cs:
-            flat_map[chain] = seq
-
-    write_fasta(flat_map, blast_db)
-
-    subprocess.check_output(
-        'makeblastdb -in ' +
-        blast_db +
-        ' -dbtype prot',
-        shell=True)
-
-
-def get_chain_sequences(pdb_file):
-    """
-    Returns list of chain sequences from PDB file
-    """
-#     fname = os.path.join(path, pdb, pdb + '_protein.pdb')
-    chain_seqs = []
-    for seq in SeqIO.parse(pdb_file, 'pdb-atom'):
-        try:
-            pdb_id = seq.idcode
-        except BaseException:
-            pdb_id = os.path.basename(pdb_file).rstrip('.pdb')
-        chain = pdb_id + '_' + seq.annotations['chain']
-        chain_seqs.append((chain, str(seq.seq)))
-    return chain_seqs
-
-
-def get_all_chain_sequences(pdb_dataset):
-    """Return list of tuples of (pdb_code, chain_sequences) for PDB dataset."""
-    return [(dt.get_pdb_code(p), get_chain_sequences(p))
-            for p in tqdm.tqdm(pdb_dataset)]
-
-
-def write_fasta(seq_dict, outfile):
-    """
-    Takes dictionary of sequences keyed by id and writes to fasta file
-    """
-    with open(outfile, 'w') as f:
-        for i, s in seq_dict.items():
-            f.write('>' + i + '\n')
-            f.write(s + '\n')
