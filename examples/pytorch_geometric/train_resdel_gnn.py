c4aadcfe208d82895c1bba040daad464973bdd8e
Alex William Derry <awfderry@gmail.com>
2020-06-04 20:21:31 -0700

    fixed resdel GNN

--- examples/pytorch_geometric/train_resdel_gnn.py
+++ examples/pytorch_geometric/train_resdel_gnn.py
@@ -16,7 +16,7 @@ sys.path.append('../..')
 import torch
 import torch.nn as nn
 from torch.utils import data
-from torch_geometric.data import Data, Batch
+from torch_geometric.data import Data, Batch, DataLoader
 from torch_geometric.nn import GCNConv, global_add_pool, DataParallel
 from torch.nn import Sequential, Linear, ReLU
 import torch.nn.functional as F
@@ -27,81 +27,22 @@ import atom3d.util.shard as sh
 import resdel_dataloader as dl
 
 
-class ResDel_Dataset(data.IterableDataset):
-    def __init__(self, sharded):
-        self.sharded = sh.load_sharded(sharded)
-        self.names = sh.get_keys(sharded)
-        self.num_shards = self.sharded.get_num_shards()
-        
-
-    def __len__(self):
-        return len(self.names)
-
-
-    def __iter__(self):
-        worker_info = torch.utils.data.get_worker_info()
-        if worker_info is None:  # single-process data loading, return the full iterator
-            gen = dl.dataset_generator(self.sharded, range(self.num_shards))
-        else:  # in a worker process
-            # split workload
-            per_worker = int(math.ceil(self.num_shards / float(worker_info.num_workers)))
-            worker_id = worker_info.id
-            iter_start = worker_id * per_worker
-            iter_end = min(iter_start + per_worker, self.num_shards)
-            gen = dl.dataset_generator(self.sharded, range(self.num_shards)[iter_start:iter_end])
-        return gen
-
-
-class Collater(object):
-    def __init__(self, follow_batch):
-        self.follow_batch = follow_batch
-
-    def collate(self, batch):
-        elem = batch[0]
-        if isinstance(elem, Data):
-            return Batch.from_data_list(batch, self.follow_batch)
-
-        raise TypeError('DataLoader found invalid type: {}'.format(type(elem)))
-
-    def __call__(self, batch):
-        return self.collate(batch)
-
-class DataLoader(data.DataLoader):
-    r"""Data loader which merges data objects from a
-    :class:`torch_geometric.data.dataset` to a mini-batch.
-
-    Args:
-        dataset (Dataset): The dataset from which to load the data.
-        batch_size (int, optional): How many samples per batch to load.
-            (default: :obj:`1`)
-        shuffle (bool, optional): If set to :obj:`True`, the data will be
-            reshuffled at every epoch. (default: :obj:`False`)
-        follow_batch (list or tuple, optional): Creates assignment batch
-            vectors for each key in the list. (default: :obj:`[]`)
-    """
-
-    def __init__(self, dataset, batch_size=1, shuffle=False, follow_batch=[],
-                 **kwargs):
-        super(DataLoader,
-              self).__init__(dataset, batch_size, shuffle,
-                             collate_fn=Collater(follow_batch), **kwargs)
-
 
 class GCN(torch.nn.Module):
     def __init__(self, num_features, hidden_dim):
         super(GCN, self).__init__()
         self.conv1 = GCNConv(num_features, hidden_dim)
-        self.bn1 = torch.nn.BatchNorm1d(hidden_dim)
-        self.conv2 = GCNConv(hidden_dim, hidden_dim)
-        self.bn2 = torch.nn.BatchNorm1d(hidden_dim)
-        self.conv3 = GCNConv(hidden_dim, hidden_dim)
-        self.bn3 = torch.nn.BatchNorm1d(hidden_dim)
-        self.conv4 = GCNConv(hidden_dim, hidden_dim)
-        self.bn4 = torch.nn.BatchNorm1d(hidden_dim)
-        self.conv5 = GCNConv(hidden_dim, hidden_dim)
-        self.bn5 = torch.nn.BatchNorm1d(hidden_dim)
-        self.fc1 = Linear(hidden_dim, hidden_dim)
-        self.fc2 = Linear(hidden_dim, 20)
+        self.bn1 = nn.BatchNorm1d(hidden_dim)
+        self.conv2 = GCNConv(hidden_dim, hidden_dim*2)
+        self.bn2 = nn.BatchNorm1d(hidden_dim*2)
+        self.conv3 = GCNConv(hidden_dim*2, hidden_dim*4)
+        self.bn3 = nn.BatchNorm1d(hidden_dim*4)
+        self.conv4 = GCNConv(hidden_dim*4, hidden_dim*4)
+        self.bn4 = nn.BatchNorm1d(hidden_dim*4)
+        self.conv5 = GCNConv(hidden_dim*4, hidden_dim*2)
+        self.bn5 = nn.BatchNorm1d(hidden_dim*2)
+        self.fc1 = Linear(hidden_dim*2, hidden_dim*2)
+        self.fc2 = Linear(hidden_dim*2, 20)
 
 
     def forward(self, x, edge_index, edge_weight, batch):
@@ -167,9 +108,7 @@ def test(model, loader, criterion, device):
     return np.mean(losses), np.mean(avg_acc), np.mean(avg_top_k_acc)
 
 
-def train(data_dir, device, log_dir, seed=None, test_mode=False):
-    # logger = logging.getLogger('resdel_log')
-    # logging.basicConfig(filename=os.path.join(log_dir, f'train_resdel.log'),level=logging.INFO)
+def train(data_dir, device, log_dir, checkpoint, seed=None, test_mode=False):
 
     epochs = 5
     batch_size = 64
@@ -183,10 +122,10 @@ def train(data_dir, device, log_dir, seed=None, test_mode=False):
             f.write(f'Batch size: {batch_size}\n')
             f.write(f'Learning rate: {learning_rate}\n')
 
-    train_set = ResDel_Dataset(os.path.join(data_dir, 'train_pdbs@1000/train_pdbs@1000'))
-    train_loader = DataLoader(train_set, batch_size=batch_size)#, num_workers=4)
-    val_set = ResDel_Dataset(os.path.join(data_dir, 'val_pdbs@100/val_pdbs@100'))
-    val_loader = DataLoader(val_set, batch_size=batch_size)#, num_workers=4)
+    train_set = dl.Resdel_Dataset_PTG(os.path.join(data_dir, 'train'))
+    train_loader = DataLoader(train_set, batch_size=batch_size, num_workers=8, shuffle=True)
+    val_set = Resdel_Dataset_PTG(os.path.join(data_dir, 'val'))
+    val_loader = DataLoader(val_set, batch_size=batch_size, num_workers=8, shuffle=True)
 
     for graph in train_loader:
         num_features = graph.num_features
@@ -200,13 +139,17 @@ def train(data_dir, device, log_dir, seed=None, test_mode=False):
     #     model = DataParallel(model)
     model.train()
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate*torch.cuda.device_count(), weight_decay=reg)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)#, weight_decay=reg)
     criterion = nn.CrossEntropyLoss()
     criterion.to(device)
 
+    if checkpoint:
+        cpt = torch.load(checkpoint, map_location=device)
+        model.load_state_dict(cpt['model_state_dict'])
+        optimizer.load_state_dict(cpt['optimizer_state_dict'])
+
     best_val_loss = 999
     best_val_idx = 0
-    validation_frequency = 10000
     print_frequency = 100
 
     for epoch in range(1, epochs+1):
@@ -223,42 +166,51 @@ def train(data_dir, device, log_dir, seed=None, test_mode=False):
             optimizer.step()
 
 
-            if it % print_frequency == 0 and it > 0:
+            if it % print_frequency == 0:
                 elapsed = time.time() - start
                 print(f'Epoch {epoch}, iter {it}, train loss {train_loss}, avg it/sec {print_frequency / elapsed}')
                 start = time.time()
-            if it % validation_frequency == 0 and it > 0: 
-                print('validating...')
-                curr_val_loss, val_acc, val_top_k_acc = test(model, val_loader, criterion, device)
-                # logger.info('{:03d}\t{}\t{:.7f}\t{:.7f}\t{:.7f}\t{:.7f}\n'.format(epoch, it, train_loss, curr_val_loss, val_acc, val_top_k_acc))
-                print('{:03d}\t{}\t{:.7f}\t{:.7f}\t{:.7f}\t{:.7f}\n'.format(epoch, it, train_loss, curr_val_loss, val_acc, val_top_k_acc))
+        print('validating...')
+        curr_val_loss, val_acc, val_top_k_acc = test(model, val_loader, criterion, device)
+        # logger.info('{:03d}\t{}\t{:.7f}\t{:.7f}\t{:.7f}\t{:.7f}\n'.format(epoch, it, train_loss, curr_val_loss, val_acc, val_top_k_acc))
+        # print('{:03d}\t{}\t{:.7f}\t{:.7f}\t{:.7f}\t{:.7f}\n'.format(epoch, it, train_loss, curr_val_loss, val_acc, v'al_top_k_acc))
+        print(f'Epoch {epoch}, iter {it}, val loss {curr_val_loss}, val acc {val_acc}, val top 3 acc {val_top_k_acc}')
 
-                if curr_val_loss < best_val_loss:
+        if curr_val_loss < best_val_loss:
 
-                    # save best validation score and iteration number
-                    best_val_loss = curr_val_loss
-                    best_val_idx = it
-                    # overwrite best model
-                    if parallel:
-                        torch.save({
-                            'epoch': epoch,
-                            'iter': it,
-                            'model_state_dict': model.module.state_dict(),
-                            'optimizer_state_dict': optimizer.state_dict(),
-                            'loss': train_loss,
-                            }, os.path.join(log_dir, f'checkpoint_epoch{epoch}_it{it}.pt'))
-                    else:
-                        torch.save({
-                            'epoch': epoch,
-                            'iter': it,
-                            'model_state_dict': model.state_dict(),
-                            'optimizer_state_dict': optimizer.state_dict(),
-                            'loss': train_loss,
-                            }, os.path.join(log_dir, f'checkpoint_epoch{epoch}_it{it}.pt'))
-                    with open(os.path.join(log_dir, 'log.txt'), 'a') as f:
-                        f.write('curr best idx \t %s\n' %best_val_idx)
+            # save best validation score and iteration number
+            best_val_loss = curr_val_loss
+            best_val_idx = it
+            # overwrite best model
+            if parallel:
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': model.module.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': train_loss,
+                    }, os.path.join(log_dir, f'checkpoint_epoch{epoch}_it{it}.pt'))
+            else:
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': train_loss,
+                    }, os.path.join(log_dir, f'checkpoint_epoch{epoch}_it{it}.pt'))
 
-                model.train()
+            model.train()
+
+    if test_mode:
+        print('testing...')
+        model = cnn_3d_new(nic=in_channels).to(device)
+        model.eval()
+        test_set = dl.Resdel_Dataset_PTG(os.path.join(data_dir, 'test_unbalanced'))
+        test_loader = DataLoader(test_set, batch_size=batch_size, num_workers=8, shuffle=True)
+        cpt = torch.load(os.path.join(log_dir, f'best_weights.pt'))
+        # cpt = torch.load(checkpoint, map_location=device)
+        model.load_state_dict(cpt['model_state_dict'])
+        test_loss, test_acc, test_top_k_acc = test(model, test_loader, criterion, device)
+        print('Test loss: {:7f}, Test Accuracy {:.4f}, Top 3 Accuracy {:4f}, F1 Score {:4f}'.format(test_loss, test_acc, test_top_k_acc, test_f1))
+        return test_loss, test_acc, test_top_k_acc
 
     return best_val_loss
 
@@ -266,18 +218,40 @@ def train(data_dir, device, log_dir, seed=None, test_mode=False):
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--mode', type=str, default='train')
-    parser.add_argument('--data_dir', type=str, default=O_DIR+'atom3d/data/residue_deletion')
     parser.add_argument('--log_dir', type=str, default=None)
+    parser.add_argument('--checkpoint', type=str, default=None)
     args = parser.parse_args()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     log_dir = args.log_dir
 
+    base_dir = '../../data/residue_deletion'
+    data_dir = SC_DIR+'atom3d/graph_pt'
 
     if args.mode == 'train':
         if log_dir is None:
             now = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-            log_dir = os.path.join(args.data_dir, 'gnn', 'logs', now)
+            log_dir = os.path.join(base_dir, 'logs_cnn', now)
+        else:
+            log_dir = os.path.join(base_dir, 'logs_cnn', log_dir)
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+        train(data_dir, device, log_dir, args.checkpoint)
+    elif args.mode == 'test':
+        test_loss_list = []
+        acc_list = []
+        f1_list = []
+        for seed in np.random.randint(0, 100, size=3):
+            print('seed:', seed)
+            log_dir = os.path.join(base_dir, 'logs_cnn', f'test_{seed}')
             if not os.path.exists(log_dir):
                 os.makedirs(log_dir)
-        train(args.data_dir, device, log_dir)
+            np.random.seed(seed)
+            torch.manual_seed(seed)
+            test_loss, test_acc, test_top_k_acc, test_f1 = train(data_dir, device, log_dir, args.checkpoint, seed=seed, test_mode=True)
+            test_loss_list.append(test_loss)
+            acc_list.append(test_acc)
+            f1_list.append(test_f1)
+        print(f'Avg test_loss: {np.mean(test_loss_list)}, St.Dev test_loss {np.std(test_loss_list)}, \
+            Avg accuracy {np.mean(acc_list)}, St.Dev accuracy {np.std(acc_list)},\
+            Avg F1 {np.mean(f1_list)}, St.Dev F1 {np.std(f1_list)}')
