380854797090dc04b9dfaba26f138c521699590b
YianniL <yianni.lalou@gmail.com>
2020-06-04 20:11:43 -0700

    Add files via upload

---
+++ atom3d/mut/addHpyro.py
@@ -0,0 +1,17 @@
+import sys
+import csv
+import os
+from glob import glob
+
+import pyrosetta
+from pyrosetta import Pose, pose_from_pdb
+
+pyrosetta.init()
+
+pdbs = glob('../cleaned/*.pdb')
+
+for f in pdbs:
+    p = pose_from_pdb(f)
+    # print(os.path.join('../cleaned_pyro', os.path.basename(f)))
+    p.dump_pdb(os.path.join('../cleaned_pyro', os.path.basename(f)))
+    print('Processed {}'.format(f))
---
+++ atom3d/mut/check_resi.py
@@ -0,0 +1,46 @@
+import sys
+import csv
+import os
+import glob
+
+import pyrosetta
+from pyrosetta import Pose, pose_from_pdb
+from pyrosetta.toolbox import mutate_residue
+
+pyrosetta.init()
+files = sorted(glob.glob('../pyrosetta_mut/*.pdb'))
+match = 0
+nomatchfiles = []
+
+jobid = int(sys.argv[1])
+num_jobs = int(sys.argv[2])
+perjob = int(len(files) / int(num_jobs))
+
+nums = list(range(len(files)))
+if jobid == num_jobs - 1:
+    indices = nums[(jobid)*perjob:]
+else:
+    indices = nums[jobid*perjob: (jobid+1)*perjob]
+print(indices)
+
+log = []
+for idx in indices:
+    s = os.path.basename(files[idx])
+    temp = s.split('_')
+    fromres = temp[-1][0]
+    chain = temp[-1][1]
+    tores = temp[-1][-5]
+    resnum = temp[-1][2:-5]
+    p = pose_from_pdb(files[idx])
+    pyro_resi = p.pdb_info().pdb2pose(chain, int(resnum))
+    pyro_resname = p.residue(pyro_resi).name1()
+    if pyro_resname == tores:
+        match += 1
+    else:
+        nomatchfiles.append(files[idx])
+    log.append([temp, fromres, chain, resnum, tores, pyro_resname])
+
+print(match, len(indices), nomatchfiles, len(nomatchfiles))
+with open('checkresiloginprogram{}.txt'.format(jobid), 'w') as f:
+    writer = csv.writer(f)
+    writer.writerows(log)
