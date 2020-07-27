ef1fd06a880a6cf18aa922fc43e360f3b040d8f7
Martin Voegele <martinvoegele1989@gmail.com>
2020-07-26 20:29:36 -0700

    adapt to new mutation data

--- examples/cormorant/prepare-mutation.sh
+++ examples/cormorant/prepare-mutation.sh
@@ -1,6 +1,7 @@
 conda activate /oak/stanford/groups/rondror/users/mvoegele/envs/atom3d 
 
-RAWDIR=../../data/mutation_prediction
+#RAWDIR=../../data/mutation_prediction
+RAWDIR=../../../../../projects/atom3d/mutation_stability_prediction
 
 for CUTOFF in 05 06 07 08 09 10 11 12 14 16 20 24 30; do
 
@@ -9,7 +10,7 @@ for CUTOFF in 05 06 07 08 09 10 11 12 14 16 20 24 30; do
 	mkdir $NPZDIR
 	mkdir $NPZDIR/mutation
 
-	python ../../atom3d/mut/convert_mutation_from_hdf5_to_npz.py $RAWDIR $NPZDIR/mutation --cutoff $CUTOFF 
+	python ../../atom3d/datasets/msp/convert_mutation_from_hdf5_to_npz.py $RAWDIR $NPZDIR/mutation --cutoff $CUTOFF 
 
 done
 
