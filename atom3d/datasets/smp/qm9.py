a9850f16534cab06a152f9e57b4da8c31cc255b0
Martin Voegele <martinvoegele1989@gmail.com>
2020-07-27 18:39:52 -0700

    QM9 ensembler

---
+++ atom3d/datasets/smp/qm9.py
@@ -0,0 +1,16 @@
+"""QM9 functions."""
+import collections as col
+from atom3d.util.formats import *
+
+def qm9_ensembler(sdf_files):
+    
+    bp = read_sdf(sdf_files)
+    df = bp_to_df(bp)
+
+    ensembles = {}
+    subunits = {}
+    for mol in df.groupby(['ensemble','model'],as_index=False):
+        subunits[mol[0][1]] = mol[1]
+    ensembles[None] = subunits
+
+    return ensembles
