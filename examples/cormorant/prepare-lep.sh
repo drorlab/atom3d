0c1863ab4945787258c22fa545df56e7525dc7c6
martinvoegele <martinvoegele1989@gmail.com>
2020-09-07 11:33:43 -0700

    LEP conversion for Cormorant

---
+++ examples/cormorant/prepare-lep.sh
@@ -0,0 +1,23 @@
+# -- Source directory paths --
+
+RAWDIR=../../data/ligand_efficacy_prediction/split
+
+# -- Reduced datasets --
+
+for MAXNUMAT in 500 480 520 540; do
+
+	for CUTOFF in 50 55 60; do
+		
+		NPZDIR=../../data/ligand_efficacy_prediction/split/npz_cutoff${CUTOFF}_maxnumat${MAXNUMAT}
+
+		mkdir $NPZDIR
+		mkdir $NPZDIR/lep
+		
+		CUTOFFNM=$( printf %.1f "$((10**1 * $CUTOFF/10))e-1" )
+		echo "Processing with cut-off $CUTOFFNM nm and max. $MAXNUMAT atoms, dropping hydrogen atoms."
+		python ../../atom3d/datasets/lep/convert_lep_to_npz.py $RAWDIR $NPZDIR/lep --cutoff $CUTOFFNM --maxnumat $MAXNUMAT --drop_h
+
+	done
+done
+
+
