846e56421670282cee6641829c172b7123bebead
Raphael Townshend <raphtown@stanford.edu>
2020-10-10 00:04:32 -0700

    Add datasets to public packages

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='atom3d',
-    packages=[
+    packages=find_packages(include=[
         'atom3d',
         'atom3d.protein',
         'atom3d.shard',
         'atom3d.util',
-    ],
+        'atom3d.datasets',
+    ]),
     version='0.1.0',
     description='ATOM3D: Tasks On Molecules in 3 Dimensions',
     author='Raphael Townshend',
