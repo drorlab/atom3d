118adead19733193e1ea31bcbcdb39eec165bcec
Raphael Townshend <raphael@tc.com>
2020-12-29 16:19:51 -0800

    Fix form_seq_filter_against.

--- atom3d/filters/sequence.py
+++ atom3d/filters/sequence.py
@@ -3,26 +3,26 @@ import pandas as pd
 import atom3d.protein.sequence as seq
 
 
-def form_seq_filter_against(sharded, cutoff):
+def form_seq_filter_against(dataset, cutoff):
     """
-    Remove structures with too much sequence identity to a chain in sharded.
+    Remove structures with too much sequence identity to a chain in dataset.
 
     We consider each chain in each structure separately, and remove the
     structure if any of them matches any chain in sharded.
     """
-    blast_db_path = f'{sharded.path:}.db'
-    all_chain_sequences = []
-    for _, shard in sharded.iter_shards():
-        all_chain_sequences.extend(seq.get_all_chain_sequences_df(shard))
-    seq.write_to_blast_db(all_chain_sequences, blast_db_path)
+    blast_db_path = f'blast_db'
+    all_chain_sequences = [seq.get_chain_sequences(x['atoms']) for x in dataset]
+    # Flatten.
+    flat_chain_sequences = [x for sublist in all_chain_sequences for x in sublist]
+    seq.write_to_blast_db(flat_chain_sequences, blast_db_path)
 
     def filter_fn(df):
-        to_keep = {}
-        for structure_name, cs in seq.get_all_chain_sequences_df(df):
-            hits = seq.find_similar(cs, blast_db_path, cutoff, 1)
-            ensemble = structure_name[0]
-            to_keep[ensemble] = (len(hits) == 0)
-        to_keep = pd.Series(to_keep)[df['ensemble']]
-        return df[to_keep.values]
+        cs = seq.get_chain_sequences(df)
+        hits = seq.find_similar(cs, blast_db_path, cutoff, 1)
+        if len(hits) > 0:
+            # Return empty dataframe.
+            return df.iloc[0:0]
+        else:
+            return df
 
     return filter_fn
