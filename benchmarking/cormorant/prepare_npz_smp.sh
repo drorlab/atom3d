bef140be9291ea0655244074d09e50fc3bb238cc
martinvoegele <martinvoegele1989@gmail.com>
2021-01-18 03:27:00 -0800

    smp npz prep

---
+++ benchmarking/cormorant/prepare_npz_smp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --time=24:00:00
+#SBATCH --mem=20G
+#SBATCH --partition=rondror
+#SBATCH --qos=high_p
+
+
+# Directory definitions
+LMDB_DIR=/oak/stanford/groups/rondror/projects/atom3d/lmdb/small_molecule_properties
+NPZ_DIR=/oak/stanford/groups/rondror/projects/atom3d/npz/smp
+
+# Create output directory
+mkdir -p $NPZ_DIR
+# Convert the dataset from LMDB to NPZ (using pre-defined split datasets)
+python ../../atom3d/datasets/smp/prepare_npz.py $LMDB_DIR $NPZ_DIR --split \
+
