26ef3558ae9d5ff46d87282674ef76cb1e6c236c
Raphael Townshend <raphael@tc.com>
2021-02-12 14:49:49 -0800

    Re-add LEP files

---
+++ atom3d/datasets/lep/label.py
@@ -0,0 +1,62 @@
+"""Label pairs as active or inactive."""
+import click
+import pandas as pd
+import parallel as par
+
+import atom3d.shard.shard as sh
+import atom3d.util.log as log
+
+logger = log.get_logger('lep_label')
+
+
+@click.command(help='Label LEP pairs with inactive/active label.')
+@click.argument('sharded_path', type=click.Path())
+@click.argument('info_csv', type=click.Path(exists=True))
+@click.option('-n', '--num_threads', default=8,
+              help='Number of threads to use for parallel processing.')
+@click.option('--overwrite/--no-overwrite', default=False,
+              help='Overwrite existing labels.')
+def gen_labels_sharded(sharded_path, info_csv, num_threads, overwrite):
+    sharded = sh.Sharded.load(sharded_path)
+    num_shards = sharded.get_num_shards()
+
+    requested_shards = list(range(num_shards))
+    if not overwrite:
+        produced_shards = [x for x in requested_shards
+                           if sharded.has(x, 'labels')]
+    else:
+        produced_shards = []
+
+    work_shards = set(requested_shards).difference(produced_shards)
+    logger.info(f'{len(requested_shards):} requested, '
+                f'{len(produced_shards):} already produced, '
+                f'{len(work_shards):} left to do.')
+    logger.info(f'Using {num_threads:} threads')
+
+    inputs = [(sharded, shard_num, info_csv)
+              for shard_num in work_shards]
+
+    par.submit_jobs(_gen_labels_shard, inputs, num_threads)
+
+
+def _gen_labels_shard(sharded, shard_num, info_csv):
+    logger.info(f'Processing shard {shard_num:}')
+    shard = sharded.read_shard(shard_num)
+    info = pd.read_csv(info_csv)
+    info['ensemble'] = info.apply(
+        lambda x: x['ligand'] + '__' + x['active_struc'].split('_')[2] + '__' +
+        x['inactive_struc'].split('_')[2], axis=1)
+    info = info.set_index('ensemble', drop=False)
+    # Remove duplicate ensembles.
+    info = info[~info.index.duplicated()]
+
+    all_labels = []
+    for e, ensemble in shard.groupby('ensemble'):
+        all_labels.append(info.loc[e][['label', 'ensemble']])
+    all_labels = pd.concat(all_labels, axis=1).T.reset_index(drop=True)
+    sharded.add_to_shard(shard_num, all_labels, 'labels')
+    logger.info(f'Done processing shard {shard_num:}')
+
+
+if __name__ == "__main__":
+    gen_labels_sharded()
---
+++ atom3d/datasets/lep/prepare.py
@@ -0,0 +1,81 @@
+"""Code for preparing a lep dataset (filtering and splitting)."""
+import click
+import pandas as pd
+
+import atom3d.filters.filters as filters
+import atom3d.shard.shard as sh
+import atom3d.shard.shard_ops as sho
+import atom3d.util.log as log
+import atom3d.splits.splits as splits
+
+logger = log.get_logger('lep_prepare')
+
+
+def split(input_sharded, output_root, info_csv, shuffle_buffer):
+    """Split by protein."""
+    if input_sharded.get_keys() != ['ensemble']:
+        raise RuntimeError('Can only apply to sharded by ensemble.')
+
+    info = pd.read_csv(info_csv)
+    info['ensemble'] = info.apply(
+        lambda x: x['ligand'] + '__' + x['active_struc'].split('_')[2] + '__' +
+        x['inactive_struc'].split('_')[2], axis=1)
+    info = info.set_index('ensemble')
+    # Remove duplicate ensembles.
+    info = info[~info.index.duplicated()]
+
+    ensembles = input_sharded.get_names()['ensemble']
+    in_use = info.loc[ensembles]
+    active = in_use[in_use['label'] == 'A']
+    inactive = in_use[in_use['label'] == 'I']
+
+    # Split by protein.
+    proteins = info['protein'].unique()
+    i_test, i_val, i_train = splits.random_split(len(proteins), 0.6, 0.2, 0.2)
+    p_train = proteins[i_train]
+    p_val = proteins[i_val]
+    p_test = proteins[i_test]
+    logger.info(f'Train proteins: {p_train:}')
+    logger.info(f'Val proteins: {p_val:}')
+    logger.info(f'Test proteins: {p_test:}')
+
+    train = info[info['protein'].isin(p_train)].index.tolist()
+    val = info[info['protein'].isin(p_val)].index.tolist()
+    test = info[info['protein'].isin(p_test)].index.tolist()
+
+    logger.info(f'{len(train):} train examples, {len(val):} val examples, '
+                f'{len(test):} test examples.')
+
+    keys = input_sharded.get_keys()
+    prefix = sh.get_prefix(output_root)
+    num_shards = sh.get_num_shards(output_root)
+    train_sharded = sh.Sharded(f'{prefix:}_train@{num_shards:}', keys)
+    val_sharded = sh.Sharded(f'{prefix:}_val@{num_shards:}', keys)
+    test_sharded = sh.Sharded(f'{prefix:}_test@{num_shards:}', keys)
+
+    train_filter_fn = filters.form_filter_against_list(train, 'ensemble')
+    val_filter_fn = filters.form_filter_against_list(val, 'ensemble')
+    test_filter_fn = filters.form_filter_against_list(test, 'ensemble')
+
+    sho.filter_sharded(
+        input_sharded, train_sharded, train_filter_fn, shuffle_buffer)
+    sho.filter_sharded(
+        input_sharded, val_sharded, val_filter_fn, shuffle_buffer)
+    sho.filter_sharded(
+        input_sharded, test_sharded, test_filter_fn, shuffle_buffer)
+
+
+@click.command(help='Prepare lep dataset')
+@click.argument('input_sharded_path', type=click.Path())
+@click.argument('output_root', type=click.Path())
+@click.argument('info_csv', type=click.Path(exists=True))
+@click.option('--shuffle_buffer', type=int, default=5,
+              help='How many shards to use in streaming shuffle. 0 means will '
+              'not shuffle.')
+def prepare(input_sharded_path, output_root, info_csv, shuffle_buffer):
+    input_sharded = sh.Sharded.load(input_sharded_path)
+    split(input_sharded, output_root, info_csv, shuffle_buffer)
+
+
+if __name__ == "__main__":
+    prepare()
