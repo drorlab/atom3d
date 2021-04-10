960a6b08604a90936abebb91426266b78f55f9f6
Martin Vögele <martinvoegele1989@gmail.com>
2021-04-10 00:25:38 -0700

    fix conversion for smp

--- examples/smp/enn/evaluate.py
+++ examples/smp/enn/evaluate.py
@@ -2,15 +2,17 @@ import numpy as np
 import atom3d.util.results as res
 import atom3d.util.metrics as met
 
-labels = np.loadtxt('labels.txt',dtype=str)
-conversion = [1., 1., 1., 1., 1., 
-              27.2114, 27.2114, 27.2114,
-              1., 27211.4, 1., 1., 1., 1., 1., 
-              23.061, 23.061, 23.061, 23.061]
+labels = np.loadtxt('labels.txt', dtype=str)
+conversion = {'A':1.0, 'B':1.0, 'C':1.0, 'mu':1.0, 'alpha':1.0,
+              'homo':27.2114, 'lumo':27.2114, 'gap':27.2114, 'r2':1.0, 'zpve':27211.4,
+              'u0':27.2114, 'u298':27.2114, 'h298':27.2114, 'g298':27.2114, 'cv':1.0,
+              'u0_atom':27.2114, 'u298_atom':27.2114, 'h298_atom':27.2114, 'g298_atom':27.2114, 'cv_atom':1.0}
 
-for cf, label in zip(conversion,labels):
+for label in labels:
+    cf = conversion[label]
     rloader = res.ResultsENN('smp-'+label, reps=[1,2,3])
     results = rloader.get_all_predictions()
     summary = met.evaluate_average(results, metric = met.mae, verbose = False)
     summary = [(cf*s[0],cf*s[1]) for s in summary]
     print('%9s: %6.3f \pm %6.3f'%(label, *summary[2]))
+
