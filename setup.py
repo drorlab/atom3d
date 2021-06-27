5a3b44b299af82ddf761b32e19307b1078416ce5
Raphael Townshend <raphtown@stanford.edu>
2021-06-27 12:14:02 -0700

    Add better versioning

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
+import os
+import re
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+
+# Recommendations from https://packaging.python.org/
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+def read(*parts):
+    with open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name='atom3d',
@@ -15,7 +35,7 @@ setup(
         'atom3d.filters',
         'atom3d.data',
     ]),
-    version='0.2.3',
+    version=find_version("atom3d", "__init__.py"),
     description='ATOM3D: Tasks On Molecules in 3 Dimensions',
     author='ATOM3D developers',
     license='MIT',
