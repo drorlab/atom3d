9fb2b33982bc4d4223353bd4f3eeb4a20bc9becf
Martin Vögele <martinvoegele1989@gmail.com>
2021-07-13 23:13:48 -0700

    remove old benchmarking

--- benchmarking/cormorant/submit-lba-id60.sh
+++
@@ -1,8 +0,0 @@
-for REP in $(seq 1 5); do
-	SB_FILE=lba-id60_rep${REP}.sb
-	cp template-lba-id60.sb ${SB_FILE}
-	sed -i "s/ --load/-rep${REP} --seed ${REP}${REP} --load/g" ${SB_FILE}
-	sed -i "s/-cormorant/-cormorant-rep${REP}/g" ${SB_FILE}
-	sbatch ${SB_FILE}
-done
-
