33d999735ac82495ab401a58a0dc73b1845a4670
Martin Vögele <martinvoegele@users.noreply.github.com>
2021-08-07 16:33:00 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 import sphinx_rtd_theme
 import mock
+import atom3d
 
 # -- Project information -----------------------------------------------------
 
